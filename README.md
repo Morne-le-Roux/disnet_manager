79f6ac091c9e3bf525dd697f7824d39a977d8e1a
Morné le Roux <mornelerx@gmail.com>
2025-05-06 16:10:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# disnet_manager
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/disnet_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.disnet_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()

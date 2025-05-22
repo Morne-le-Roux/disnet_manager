71c2e5ed86ac1e2a79d44db0668c824747ae49f3
Morné le Roux <mornelerx@gmail.com>
2025-05-22 17:44:18 +0200

    initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+import 'package:disnet_manager/usecases/init_pb.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:disnet_manager/usecases/init_env.dart';
+import 'package:disnet_manager/usecases/run_app_with_wrappers.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Hydrated Bloc Storage
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: HydratedStorageDirectory(
+        (await getApplicationDocumentsDirectory()).path),
+  );
+
+  await initEnv();
+  await initPB();
+
+  runAppWithWrappers(MainApp());
 }
 
 class MainApp extends StatelessWidget {
